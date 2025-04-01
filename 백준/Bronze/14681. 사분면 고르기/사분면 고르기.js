a8b50978925a01548e7471310eb70bd64fbbf5b9
YouVin <66316777+YouVin@users.noreply.github.com>
2025-04-01 17:04:48 +0900

    [Bronze V] Title: 사분면 고르기, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const inputData = fs.readFileSync(0).toString().trim().split("\n");
+// console.log(inputData);
+
+const x = parseInt(inputData[0]);
+const y = parseInt(inputData[1]);
+
+if(x > 0 && y > 0){
+  console.log(1);
+} else if(x > 0 && y < 0){
+  console.log(4);
+} else if(x < 0 && y < 0){
+  console.log(3);
+}
+else{
+  console.log(2);
+}
