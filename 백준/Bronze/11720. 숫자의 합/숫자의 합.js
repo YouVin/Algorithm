6c3116088b3d3976eaca53d2a2c38a12f7c939e7
YouVin <66316777+YouVin@users.noreply.github.com>
2025-04-15 16:26:29 +0900

    [Bronze IV] Title: 숫자의 합, Time: 96 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);        
+const numbers = input[1];          
+
+let sum = 0;
+for (let i = 0; i < n; i++) {
+  sum += Number(numbers[i]);
+}
+
+console.log(sum);
