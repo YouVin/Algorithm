f471bf8efd1a866fea22bdcf6d2df30c95715e5d
YouVin <66316777+YouVin@users.noreply.github.com>
2025-05-03 02:53:44 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 104 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim();
+const input = fs.readFileSync(0).toString().trim(); 
 
-const counts = Array(26).fill(0);
+const counts = Array(26).fill(0); 
 
 for (let char of input) {
-  const index = char.charCodeAt(0) - 97; // 'a' ~ 'z' → 0 ~ 25
-  if (index >= 0 && index <= 26) {       // ❌ 여기! index가 26일 수는 없음!
-    counts[index]++;
-  }
+  const index = char.charCodeAt(0) - 97; 
+  counts[index]++;
 }
 
 console.log(counts.join(' '));
